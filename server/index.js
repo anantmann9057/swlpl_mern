698728a1bdcbe2ecb73f764f32aa347d7f62230e
anantmann <anantmann9057@gmail.com>
2025-03-04 13:29:40 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config({ path: "../.env" });
 
 dbConnect();
 app.use(
-  cors({ origin:'http://localhost:5173', credentials: true })
+  cors({ origin:'https://swlpl-mern-1.onrender.com', credentials: true })
 );
 app.use(express.json({ limit: "50mb" }));
 // app.use(cookieParser());
